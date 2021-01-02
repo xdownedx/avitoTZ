5a44aa9429b48abaa73bae9b313f23a2d83aa913
Max Palehin <ars_kit@icloud.com>
2021-01-02 14:04:24 +0300

    Initial Commit

---
+++ avitoTZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avitoTZ
+//
+//  Created by Максим Палёхин on 02.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
